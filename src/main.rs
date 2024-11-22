ab59949c3b8616a015961dc57d6768335cc2474b
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-11-21 19:30:02 -0500

    preparing the CI

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,12 @@ fn main() {
     println!("Hello, world!");
     println!("this is a crossword puzzle!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn dummy_unit_test() {
+        assert_eq!(1, 1);
+    }
+}
