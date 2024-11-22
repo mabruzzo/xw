2c35003f9653a445081f3223e5ac38d8dfd5a725
Matthew Abruzzo <matthewabruzzo@gmail.com>
2024-11-21 20:32:03 -0500

    using Option<char> within Grid

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@ use ndarray::prelude::*;
 
 #[derive(Debug)]
 pub struct Grid {
-    grid: Array2<char>,
+    grid: Array2<Option<char>>,
 }
 
 impl Grid {
     pub fn new() -> Grid {
         Grid {
-            grid: Array::from_elem((3, 4), ' '),
+            grid: Array::from_elem((3, 4), None),
         }
     }
 }
